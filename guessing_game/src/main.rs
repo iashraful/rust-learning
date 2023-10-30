6cf459ff6bff2d6d7af216771ec5ea3ee955fb67
Mohammad Ashraful <ashrafulrobin3@gmail.com>
2023-10-30 16:52:45 +0600

    Added more logic and control

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -7,21 +7,40 @@ fn main() {
     println!("========== Gueesing Game ==========");
     println!("-----------------------------------");
 
-    let random_num: u8 = rand::thread_rng().gen_range(1..=100);
+    let random_num: u32 = rand::thread_rng().gen_range(1..=100);
+    let mut guess_counter: u32 = 0;
 
-    println!("Guess your number: ");
-    let mut tmp: String = String::new(); // This just allocate a memory location with empty/null value.
-    io::stdin()
-        .read_line(&mut tmp)
-        .expect("Failed to get the input.");
+    loop {
+        println!("Guess your number: ");
+        let mut tmp: String = String::new(); // This just allocate a memory location with empty/null value.
+        io::stdin()
+            .read_line(&mut tmp)
+            .expect("Failed to get the input.");
 
-    let guess: u8 = tmp.trim().parse().expect("Expecting a number.");
-    println!("Secrect number is: {random_num}");
-    println!("You guessed: {guess}");
+        let guess: u32 = match tmp.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid Input provided.");
+                guess_counter += 1;
+                continue;
+            }
+        };
+        // println!("Secrect number is: {random_num}");
+        // println!("You guessed: {guess}");
 
-    match guess.cmp(&random_num) {
-        Ordering::Equal => println!("You won!"),
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
+        match guess.cmp(&random_num) {
+            Ordering::Equal => {
+                println!("Congrats. You won! Tried {guess_counter} time(s).");
+                break;
+            }
+            Ordering::Less => {
+                println!("Too small!");
+                guess_counter += 1;
+            }
+            Ordering::Greater => {
+                println!("Too big!");
+                guess_counter += 1;
+            }
+        }
     }
 }
