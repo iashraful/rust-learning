ab28065e2076fe62afdebaca517f765943b51f61
Mohammad Ashraful <ashrafulrobin3@gmail.com>
2023-10-30 16:23:56 +0600

    Added random number generation and proper types

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,13 +1,27 @@
-use std::io;
+use std::{cmp::Ordering, io};
+
+use rand::Rng;
 
 fn main() {
     println!("-----------------------------------");
     println!("========== Gueesing Game ==========");
     println!("-----------------------------------");
+
+    let random_num: u8 = rand::thread_rng().gen_range(1..=100);
+
     println!("Guess your number: ");
-    let mut guess: String = String::new(); // This just allocate a memory location with empty/null value.
+    let mut tmp: String = String::new(); // This just allocate a memory location with empty/null value.
     io::stdin()
-        .read_line(&mut guess)
+        .read_line(&mut tmp)
         .expect("Failed to get the input.");
+
+    let guess: u8 = tmp.trim().parse().expect("Expecting a number.");
+    println!("Secrect number is: {random_num}");
     println!("You guessed: {guess}");
+
+    match guess.cmp(&random_num) {
+        Ordering::Equal => println!("You won!"),
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+    }
 }
