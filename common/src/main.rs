1e56efe52e508dec35073533e7d96a45490401f0
Mohammad Ashraful <a5hraful@outlook.com>
2023-11-02 00:09:31 +0600

    Function introduced

--- common/src/main.rs
+++ common/src/main.rs
@@ -8,4 +8,10 @@ fn main() {
     // Number literals
     a = 1_000;
     println!("1000 and 1_000 are same as {a}");
+    // Another function called
+    another_function();
+}
+
+fn another_function() {
+    println!("This is from another function");
 }
